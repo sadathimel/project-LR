1cc6d8444aaf93af33f2d6bdb8c000da31f69b67
sadat <sadat.himel@gmail.com>
2021-01-10 23:59:19 +0600

    this is index file

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+    include 'inc/header.php';
+    include 'lib/User.php';
+    $user = new User();
+?>
+
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h2>User list <sapn class="pull-right" ><strong>Welcome!</strong>Himel</sapn></h2>
+            </div>
+            <div class="panel-body">
+                <table class="table table-striped">
+                   <tr>
+                       <th width="20%">Serial</th>
+                       <th width="20%">Name</th>
+                       <th width="20%">Username</th>
+                       <th width="20%">Email</th>
+                       <th width="20%">Action</th>
+                   </tr>
+
+                    <tr>
+                        <td>01</td>
+                        <td>Sadat Himel</td>
+                        <td>Himel</td>
+                        <td>sadat.himel@gmail.com</td>
+                        <td>
+                            <a class="btn btn-primary" href="profile.php?id=1">View</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>02</td>
+                        <td>Tanvir ahmed opel</td>
+                        <td>opel</td>
+                        <td>tanvir@gmail.com</td>
+                        <td>
+                            <a class="btn btn-primary" href="profile.php?id=1">View</a>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+
+        <?php
+            include 'inc/footer.php';
+        ?>
